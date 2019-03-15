046a98c40f7a0852e2c6ea690fc8ea2af76f1e15
peilin jiang <jpl199105@gmail.com>
2019-03-15 14:46:20 -0400

    practice

---
+++ Python/test.py
@@ -0,0 +1,17 @@
+def calculateProfit(n, earning, cost, e):
+	money = 0
+	for i in range(n):
+		if(i==n-1):
+			money += e*earning[i]
+			return money
+		if(earning[i]>cost[i]):
+			money += e*earning[i]
+			money = money-e*cost[i]
+	return money
+
+n , earning, cost, e = 4, [1,8,6,7],[1,3,4,1], 5
+
+money = calculateProfit(n, earning, cost,e)
+
+print(money)
+
