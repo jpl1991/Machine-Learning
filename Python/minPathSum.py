f9b67bbedc624ae80d7738278782ce6dbcdbcc16
Peilin Jiang <jpl199105@gmail.com>
2019-04-13 16:43:42 -0400

    deep learning

--- Python/minPathSum.py
+++ Python/minPathSum.py
@@ -1,8 +1,10 @@
+#top down:
 def minPathSum(grid):
     cols = len(grid[0])
     rows = len(grid)
 
     new_grid = [[0 for _ in range(cols)] for _ in range(rows)]
+    new_grid[0][0] = grid[0][0]
 
     for row in range(rows):
         for col in range(cols):
@@ -26,3 +28,48 @@ grid = [
 output = minPathSum(grid)
 
 print("Minimum:",output)
+
+#down top:
+def minPathSum2(grid):
+    cols = len(grid[0])
+    rows = len(grid)
+
+    new_grid = [[0 for _ in range(cols)] for _ in range(rows)]
+    new_grid[rows-1][cols-1] = grid[rows-1][cols-1]
+    for row in range(rows-1,-1,-1):
+
+        for col in range(cols-1,-1,-1):
+
+            if row == rows-1 and col!=cols-1:
+                new_grid[row][col] = grid[row][col] + new_grid[row][col+1]
+
+            if col == cols-1 and row!= rows-1:
+                new_grid[row][col] = grid[row][col] + new_grid[row+1][col]
+
+            if col!=cols-1 and row!=rows-1:
+                new_grid[row][col] = grid[row][col] + min(new_grid[row+1][col], new_grid[row][col+1])
+
+    return new_grid
+
+output2 = minPathSum2(grid)
+print("down to top minimun: ", output2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
